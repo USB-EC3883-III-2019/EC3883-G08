eb6cc40efb9d01c62452acacfff280fb0d113813
Hugo Troyani <hugotroyani@gmail.com>
2019-10-20 21:47:42 -0400

    Motor avanza de ida y vuelta

--- README.md
+++
@@ -1,8 +0,0 @@
-# SOLINDAR
-
-## Integrantes del equipo
-* 14-10281 Etna Patricia Davis Osorio
-* 14-11094 Hugo Leonardo Troyani Seijas
-
-
-
---
+++ SOLINDAR/FLASH/Generated_Code/PWM1_c.d
@@ -0,0 +1,11 @@
+
+Generated_Code/PWM1_c.obj: \
+	C:/Users/Hugo/workspace/SOLINDAR/Project_Headers/../Generated_Code/PWM1.c \
+	C:/Users/Hugo/workspace/SOLINDAR/Generated_Code/PE_Error.h \
+	C:/Users/Hugo/workspace/SOLINDAR/Generated_Code/PWM1.h \
+	C:/Users/Hugo/workspace/SOLINDAR/Generated_Code/PE_Types.h \
+	C:/Users/Hugo/workspace/SOLINDAR/Generated_Code/PE_Const.h \
+	C:/Users/Hugo/workspace/SOLINDAR/Generated_Code/IO_Map.h \
+	C:/Users/Hugo/workspace/SOLINDAR/Generated_Code/PE_Timer.h \
+	C:/Users/Hugo/workspace/SOLINDAR/Generated_Code/Cpu.h 
+
